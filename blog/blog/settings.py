7d4958eaa5906a71e7a4f9123e68abbb92ef0ba0
Moises Santos <mzsantoz@outlook.com>
2021-02-16 17:53:02 -0300

    Adjusting envinroment variables

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'la5ax=+a5u83i%r%yan8wb7b!5%)bk^cxt8m5ali!j+&5s-vny'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- blog/website/views.py
+++ blog/website/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 def hello_blog(request):
-  return render(request, 'index.html')
+  
+  post_list = Post.objects.all()
+  
+  data = { 
+    'posts': post_list 
+    }
+
+  return render(request, 'index.html', data)
